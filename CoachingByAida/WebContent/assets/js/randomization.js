65443f2db9ea6a1ae0f447693002095d0ff3de29
Blejzer <nikola.kujaca@gmail.com>
2014-07-31 17:12:08 +0200

    Napravio trazene izmjene na index.html: font i velicina dodao sliku i pozicionirao je (css) ... poceo raditi na services

---
+++ CoachingByAida/WebContent/assets/js/randomization.js
@@ -0,0 +1,12 @@
+$(function() {
+			var images = [ 'sunrise01.jpg', 'sunrise02.jpg', 'sunrise03.jpg',
+					'sunrise04.jpg', 'sunrise05.jpg', 'sunrise06.jpg',
+					'sunrise07.jpg', 'sunrise08.jpg', 'sunrise09.jpg',
+					'sunrise10.jpg', 'sunrise11.jpg', 'sunrise12.jpg' ];
+			$('#home1').css(
+					{
+						'background-image' : 'url(assets/img/cover/'
+								+ images[Math.floor(Math.random()
+										* images.length)] + ')'
+					});
+		});
