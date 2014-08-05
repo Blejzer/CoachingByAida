706be6345ce807468f27cbedbcf17a18ec942630
Blejzer <nikola.kujaca@gmail.com>
2014-08-05 16:39:12 +0200

    uradio stiliziranje slike, promijenio pozadinu na footerBa, dodao indexBA, servicesBA. smanjio randomization.js i styleIndexBA

--- CoachingByAida/WebContent/assets/js/randomization.js
+++ CoachingByAida/WebContent/assets/js/randomization.js
@@ -1,12 +1,1 @@
-$(function() {
-	var images = [ 'sunrise01.jpg', 'sunrise02.jpg', 'sunrise03.jpg',
-	               'sunrise05.jpg', 'sunrise06.jpg', 'sunrise07.jpg',
-	               'sunrise08.jpg', 'sunrise09.jpg', 'sunrise10.jpg', 
-	               'sunrise12.jpg' ];
-	$('#home1').css(
-			{
-				'background-image' : 'url(assets/img/cover/'
-					+ images[Math.floor(Math.random()
-							* images.length)] + ')'
-			});
-});
+$(function(){var images=['sunrise01.jpg','sunrise02.jpg','sunrise03.jpg','sunrise05.jpg','sunrise06.jpg','sunrise07.jpg','sunrise08.jpg','sunrise09.jpg','sunrise10.jpg','sunrise12.jpg'];$('#home1').css({'background-image':'url(assets/img/cover/'+images[Math.floor(Math.random()* images.length)]+')'});});
